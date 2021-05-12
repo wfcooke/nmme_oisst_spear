f07cd77eba8103672d05394772da4d0767e02d68
Colleen McHugh <colleen.mchugh@noaa.gov>
2021-05-12 10:43:40 -0400

    Only download new data if ok file doesn't exist

--- bin/get_oisst.sh
+++ bin/get_oisst.sh
@@ -5,6 +5,7 @@ umask 022
 iy=$(date +'%Y') #current year
 
 iy_prev=$( date -d "$(date +%Y-%m-15) -1 month" +'%Y' ) #year of the previous month
+mmyyyy=$( date -d "$(date +%Y-%m-15) -1 month" +'%Y%m' )
 mm=$( date -d "$(date +%Y-%m-15) -1 month" +'%^b%Y' )
 machine=ftp.cdc.noaa.gov
 
@@ -14,22 +15,19 @@ if [ ! -d ${dout} ]; then
   mkdir -p ${dout}
 fi
 
+#check to see if data has been processed yet
+ok_file=/local2/home/NMME/oisst_spear/NetCDF/${mmyyyy}.OK
+
+if [ -e ${ok_file} ]; then
+    echo "${ok_file} exists, data already processed for this month."
+    exit 0
+else
+    echo "${ok_file} does not exist, downloading raw data to ${dout}."
+fi
+
+#download data
 cd $dout
 
-if [[ ${iy} == ${iy_prev} ]]; then
-echo "Downloading data for ${iy}"
-
-ftp -n $machine << --          || exit
-user anonymous Oar.Gfdl.Nmme@noaa.gov
-cd Datasets/noaa.oisst.v2.highres
-prompt
-binary
-mget icec.day.mean.${iy}.v2.nc
-mget sst.day.mean.${iy}.v2.nc
-quit
---
-
-elif [[ ${iy} != ${iy_prev} ]]; then
 echo "Downloading data for ${iy} and ${iy_prev}"
 
 ftp -n $machine << --          || exit
@@ -44,4 +42,5 @@ mget sst.day.mean.${iy_prev}.v2.nc
 quit
 --
 
-fi
+echo "Finished downloading data"
+
